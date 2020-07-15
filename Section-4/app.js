2482fb496549431ec51268e1ef538c552227f3e3
Ziaulhaq <ziaullhaq11@gmail.com>
2020-07-15 20:55:59 +0530

    mobile

--- Section-4/app.js
+++ Section-4/app.js
@@ -1,4 +1,4 @@
-var scores,roundScore,activePlayer;
+var scores,roundScore,activePlayer,isPlaying;
 
 init();
 
@@ -28,13 +28,14 @@ function nextPlayer() {
 so its a CALLBACK function. And another thing is we can write function here itself but it  will be anonymous function. Since we are not giving
 any name and we cant use that function anymore.*/
 document.querySelector('.btn-roll').addEventListener('click', function() {   //1.event type 2.event
+  if (isPlaying) {
     //1.Random number
-    var dice=Math.floor(Math.random() * 6) +1;
+    var dice = Math.floor(Math.random() * 6) + 1;
 
     //2.Display the result
     var diceDom = document.querySelector('.dice')
     diceDom.style.display = 'block';
-    diceDom.src = 'dice-'+ dice + '.png'
+    diceDom.src = 'dice-' + dice + '.png'
 
     //3.Update the round score if the rolled number was not 1
     if (dice !== 1) {
@@ -42,34 +43,40 @@ document.querySelector('.btn-roll').addEventListener('click', function() {   //1
       roundScore += dice;
       //Displaying score
       document.querySelector('#current-' + activePlayer).textContent = roundScore;
-    }else {
+    } else {
       nextPlayer();
     }
+  }
+    
 })
 
 
 document.querySelector('.btn-hold').addEventListener('click', function() {
-  //Add current score to global score
-  scores[activePlayer] += roundScore;
+  if (isPlaying) {
+    //Add current score to global score
+    scores[activePlayer] += roundScore;
 
-  //Update the UI
-  document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+    //Update the UI
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-  //Check if player won the game
-/*  if (scores[activePlayer] >= 20) {
-    document.querySelector('#name-' + activePlayer).textContent = 'Player' + activePlayer + 'wins the game.'
-  }*/
+    //Check if player won the game
+    /*  if (scores[activePlayer] >= 20) {
+        document.querySelector('#name-' + activePlayer).textContent = 'Player' + activePlayer + 'wins the game.'
+      }*/
 
-  if (scores[activePlayer] >= 20) {
-    document.querySelector('#name-' + activePlayer).textContent = 'Winner';
-    var diceDom = document.querySelector('.dice')
-    diceDom.style.display = 'none';
-    document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
-    document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
-  }else {
-    //Next Player
-    nextPlayer();
+    if (scores[activePlayer] >= 20) {
+      document.querySelector('#name-' + activePlayer).textContent = 'Winner';
+      var diceDom = document.querySelector('.dice')
+      diceDom.style.display = 'none';
+      document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
+      document.querySelector('.player-' + activePlayer + '-panel').classList.remove('active');
+      isPlaying = false;
+    } else {
+      //Next Player
+      nextPlayer();
+    }
   }
+  
 })
 
 
@@ -81,7 +88,7 @@ function init() {
   scores = [0,0];
   roundScore = 0;
   activePlayer = 0;
-
+  isPlaying = true;
   /*for random generator we are using Math.random() function but it will return value b/w 0 & 1 . so to fix this we are multiplying the value with
   6 then we get 4.62654 like value . so we want integer not float. So we use Math.floor to convert float to int. But still it is giving the value
   from 0 to 6 to solve this we are adding +1 to the function so it will not be 0*/
@@ -98,7 +105,10 @@ function init() {
   document.getElementById('current-0').textContent = '0';
   document.getElementById('current-1').textContent = '0';
   document.querySelector('.player-0-panel').classList.remove('winner');
+  document.querySelector('.player-1-panel').classList.remove('winner');
+  document.querySelector('.player-0-panel').classList.remove('active');
   document.querySelector('.player-1-panel').classList.remove('active');
+  document.querySelector('.player-0-panel').classList.add('active');  //i am adding here since it is calling in the first. so we need to put active for the first player otherwise both will be the active player since we use toggle method
   document.querySelector('#name-0').textContent = 'Player 1';
   document.querySelector('#name-1').textContent = 'Player 2';
 }
