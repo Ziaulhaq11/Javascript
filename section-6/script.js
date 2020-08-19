19877cf049655f03b1da3f74931f95d87002669b
Ziaulhaq <ziaullhaq11@gmail.com>
2020-08-19 20:48:30 +0530

    done

--- section-6/script.js
+++ section-6/script.js
@@ -5,6 +5,19 @@ var budgetController = (function () {
         this.id = id;
         this.description = description;
         this.value = value;
+        this.percentage = -1;
+    };
+
+    Expense.prototype.calcPercentage = function(totalIncome) {
+        if (totalIncome > 0) {
+            this.percentage = Math.round((this.value / totalIncome) * 100);
+        } else {
+            this.percentage = -1;
+        } 
+    };
+
+    Expense.prototype.getPercentage = function() {
+        return this.percentage;
     }
 
     var Income = function (id,description,value) {
@@ -36,24 +49,24 @@ var budgetController = (function () {
 
     return {
         addItem : function(type, des, val) {
-            var newItem, ID;
+            var newItem, id;
             
-            //  [1 2 3 4 5] , next ID = 6 
-            // [1 2 4 6 8], next ID = 9  Because we may deleted some of the middle id's then if we just add the length then the other will be 6.
-            // ID = last ID + 1  here 8 is at 4th place then the next 
+            //  [1 2 3 4 5] , next id = 6 
+            // [1 2 4 6 8], next id = 9  Because we may deleted some of the middle id's then if we just add the length then the other will be 6.
+            // id = last id + 1  here 8 is at 4th place then the next 
 
-            //Create new ID 
+            //Create new id 
             if (data.allitems[type].length > 0) {
-                ID = data.allitems[type][data.allitems[type].length - 1].id + 1;//exp[1,4,6] => exp[3 -1]=>exp[2]=> 6id => 2 + 1 => 3;
+                id = data.allitems[type][data.allitems[type].length - 1].id + 1;//exp[1,4,6] => exp[3 -1]=>exp[2]=> 6id => 2 + 1 => 3;
                 }else {
-                ID = 0;
+                id = 0;
             }
 
             // Create new item based on 'inc' or 'exp' type
             if (type === 'exp') {
-                newItem = new Expense(ID, des, val);
+                newItem = new Expense(id, des, val);
             }else if (type === 'inc') {
-                newItem = new Income(ID, des, val);
+                newItem = new Income(id, des, val);
             }
 
             //Push it into our data structure
@@ -65,21 +78,23 @@ var budgetController = (function () {
 
         deleteItem : function (type,id) {
             var ids, index;
+            //data.allitems[type][id];  We cant use them since the id's will not be in order.
             //id = 6
             //data.allitems[type][id]
             //[1 2 4 6 8]   
             //index = 3
-
-            ids = data.allitems[type].map(function(current) {
+            console.log(type,id);
+            ids = data.allitems[type].map(function(current) {  //current represents to the current element
+                //the difference b/w map and foreach is map returns a brand new array. Where as we need to declare a variable for Foreach.  
                 return current.id;
-            })
+            });
+            console.log(ids,id);
+            index = ids.indexOf(id); //for ex : The ids variable stores all of ids and when we pass our id it will return the index of our id.
+            console.log(index);
 
-            
-            index = ids.indexOf(id);
-            return console.log(ids,index);
-
-            if(index !== -1) {
-                data.allitems[type].splice()
+            //this exist because if item is not there it will return -1
+            if(index !== -1) {  
+                data.allitems[type].splice(index, 1)  //to remove the element
             }
             
 
@@ -100,6 +115,25 @@ var budgetController = (function () {
             console.log(data.totals.inc,data.totals.exp,data.percent);
 
         },
+
+        calculatePercentage : function() {
+            /*
+            a = 10, b =20 , c= 40
+            income = 100
+            a=10/100 => 10%, b= 20% ,c= 40%
+            */
+
+            data.allitems.exp.forEach(function (cur) {
+               cur.calcPercentage(data.totals.inc); 
+            });
+        },
+
+        getPercentages : function() {
+            allPercentages = data.allitems.exp.map(function(cur) {
+                return cur.getPercentage();
+            });
+            return allPercentages;
+        } ,
         getBudget : function() {
             return {
                 budget : data.budget,
@@ -130,8 +164,37 @@ var UIController = (function () {
         incomeLabel: '.budget__income--value',
         expenseLabel: '.budget__expenses--value',
         percentageLabel: '.budget__expenses--percentage',
-        container: '.container'
+        container: '.container',
+        itemPercentage: '.item__percentage'
     }
+
+    var formatNumber = function(num, type) {
+        /*
+        + or - before number
+        two decimal points
+        separating with , in thousands
+        2354.4567  ->  + 2354.46
+        2000  ->  + 2000.00
+        */
+
+        num = Math.abs(num);
+        num = num.toFixed(2);   //it returns string.
+
+        numSplit = num.split('.')
+        int = numSplit[0];
+
+        if (int.length > 3) {
+            //int = int.substr(0,1) + ',' + int.substr(1,3);  input : 2546   output : 2,546
+            int = int.substr(0, int.length - 3) + ',' + int.substr(int.length - 3, 3);
+        };
+        dec = numSplit[1];
+
+        /*type === 'exp' ? sign = '-' : sign = '+';
+        return type + ' ' + int + dec;*/
+        return (type === 'exp' ? '-' : '+') + ' ' + int + '.' + dec;  //both are same
+    };
+
+    //Previously formatNumber is in return. But we moved it above and for using we just use without 'this' keyword.
 
     return {
         getInput : function () {  //previously it was = but error occurs
@@ -157,10 +220,15 @@ var UIController = (function () {
             //Replace the placeholder text with some actual data
             newHtml = html.replace('%id%', obj.id);
             newHtml = newHtml.replace('%description%',obj.description);
-            newHtml = newHtml.replace('%value%', obj.value);
+            newHtml = newHtml.replace('%value%', formatNumber(obj.value, type));
             //Insert some Html data into the DOM
             document.querySelector(element).insertAdjacentHTML('beforeend', newHtml);  //so beforeend means child of it.
 
+        },
+
+        deleteListItem : function(selectorID) {
+            var el = document.getElementById(selectorID);
+            el.parentNode.removeChild(el);
         },
 
         clearFields : function () {
@@ -177,9 +245,11 @@ var UIController = (function () {
         },
 
         displayBudget : function(obj) {
-            document.querySelector(DOMStrings.budgetLabel).textContent = obj.budget;
-            document.querySelector(DOMStrings.incomeLabel).textContent = obj.totalInc;
-            document.querySelector(DOMStrings.expenseLabel).textContent = obj.totalExp;
+            var type;
+            obj.budget > 0 ? type = 'inc' : type = 'exp';
+            document.querySelector(DOMStrings.budgetLabel).textContent = formatNumber(obj.budget,type);
+            document.querySelector(DOMStrings.incomeLabel).textContent = formatNumber(obj.totalInc,'inc');
+            document.querySelector(DOMStrings.expenseLabel).textContent =formatNumber( obj.totalExp, 'exp');
             if (obj.percent > 0) {
                 document.querySelector(DOMStrings.percentageLabel).textContent = obj.percent + '%';    
             }else {
@@ -187,6 +257,25 @@ var UIController = (function () {
             }
             
 
+        },
+
+        displayPercentages : function(percentages) {
+            var fields = document.querySelectorAll(DOMStrings.itemPercentage); //node list not array
+            
+            var nodeListForEach = function(list, callback) {
+                for (var i = 0; i < list.length; i++) {
+                    callback(list[i], i);
+                }
+            };
+
+            nodeListForEach(fields, function (current, index) {  //calling the above function using these parameters.
+                if (percentages[index] > 0) {
+                    current.textContent = percentages[index] + '%';
+                } else {
+                    current.textContent = '---';
+                }
+                
+            })
         },
 
         getDomstrings : function () {
@@ -222,6 +311,16 @@ var Controller = (function (budgetctrl, Uictrl) {
         Uictrl.displayBudget(budget);
     }
 
+    var updatePercentages = function() {
+        //1.Calculate the Percentages
+        budgetctrl.calculatePercentage();
+        //2.Read the percentages from the budget Controller
+        var percentages = budgetctrl.getPercentages();
+        //3.Display the new percentages on the UI
+        Uictrl.displayPercentages(percentages);
+
+    }
+
     var ctrlAddItem = function () {
         var input,newItem;
         //1.Get input data
@@ -236,14 +335,15 @@ var Controller = (function (budgetctrl, Uictrl) {
             Uictrl.clearFields();
             //5.Calculate and Update the budget
             updateBudget();
+            //6.Calculating and Updating the new Percentages
+            updatePercentages();
 
         } 
         
     };
 
     var ctrlDeleteItem = function (event) {
-        item = budgetController();
-        var itemID;
+        var itemID,splitID,type,id;
         /*console.log(event.target);
         itemID2 = event.target.id;
         console.log(itemID2);*/
@@ -254,8 +354,21 @@ var Controller = (function (budgetctrl, Uictrl) {
             //inc-1
             splitID = itemID.split('-');
             type = splitID[0];
-            id = splitID[1];
+            id = parseInt(splitID[1]);  // its a string
+            console.log(type,id);   
         }
+
+        //1.Delete the item from the data structure
+        budgetctrl.deleteItem(type,id);
+
+        //2.Delete the item from UI
+        Uictrl.deleteListItem(itemID);
+
+        //3. Update and Show the new budget.
+        updateBudget(); 
+
+        //4.Calculate and update new Percentages
+        updatePercentages();
     }
 
     return {
--- section-6/testing.js
+++ section-6/testing.js
@@ -1,4 +1,30 @@
-var a = 5;
+/*function setup() {
+    // Write your code here.
+}
 
-b = isNaN(a);
-console.log(b);
+// Example case. 
+document.body.innerHTML = `
+<div class="image">
+  <img src="https://goo.gl/kjzfbE" alt="First">
+  <button class="remove">X</button>
+</div>
+<div class="image">
+  <img src="https://goo.gl/d2JncW" alt="Second">
+  <button class="remove">X</button>
+</div>`;
+
+setup();
+
+document.getElementsByClassName("remove")[0].click();
+console.log(document.body.innerHTML);*/
+
+function format(userDate) {
+    splitDate = userDate.split('/');
+    newDate = splitDate[2] + splitDate[0] + splitDate[1];
+    return newDate;
+}
+
+console.log(format('12/31/2014'));
+console.log(format('13/08/2020'));
+
+//user date has to be strings and result also. And final result will look like '20141231'.
